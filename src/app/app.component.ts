ccb4580be8381b6bc8c4cecd510fda36b6c41219
Nermin <talibovanermin199@gmail.com>
2020-12-03 20:51:21 +0400

    fromEvent_usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'observables';
+
+  // creating observable
+  clicking$ = fromEvent(document, 'click');
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+    // subscribe to observable
+    this.clicking$.subscribe(event => console.log(event));
+  }
+
+
 }
